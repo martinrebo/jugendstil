01761842e4d87815b178ac4ad0550c3a18e19853
martinrebo <martinrg33@gmail.com>
2020-10-10 15:00:31 +0200

    Updates

---
+++ precache-manifest.3261468151e47415cc1743665cf00fe7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49acf30dc55062a4a98d17eaf875e150",
+    "url": "/jugendstil/index.html"
+  },
+  {
+    "revision": "2d1046053e918ad94fff",
+    "url": "/jugendstil/static/css/main.f6553906.chunk.css"
+  },
+  {
+    "revision": "c91cbf0162d6d3b42130",
+    "url": "/jugendstil/static/js/2.fe3f1fb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jugendstil/static/js/2.fe3f1fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1046053e918ad94fff",
+    "url": "/jugendstil/static/js/main.3c798033.chunk.js"
+  },
+  {
+    "revision": "efb787b7bb9f5ce2493a",
+    "url": "/jugendstil/static/js/runtime-main.e01554aa.js"
+  },
+  {
+    "revision": "fd5e94fd855b3ba75898346f67eed5af",
+    "url": "/jugendstil/static/media/circle.fd5e94fd.png"
+  },
+  {
+    "revision": "6e3c34d507f7a7a6ee1d69c9863619a4",
+    "url": "/jugendstil/static/media/geometry.6e3c34d5.gif"
+  },
+  {
+    "revision": "9e3a544e06a80414744009d90351b4d4",
+    "url": "/jugendstil/static/media/geometry2.9e3a544e.gif"
+  }
+]);
